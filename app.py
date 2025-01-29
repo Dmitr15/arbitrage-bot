ffb1d86ed92e8027c106887473fec00129c8eabf
DM1 <dmitroy1234@gmail.com>
2025-01-29 22:15:03 +0300

    init

--- app.py
+++ app.py
@@ -4,3 +4,16 @@ import ccxt
 import pandas as pd
 import pybit
 from pybit.unified_trading import HTTP
+
+#Necessary exchanges
+
+bingx = ccxt.bingx()
+bitbank = ccxt.bitbank()
+bitfinex = ccxt.bitfinex()
+bitget = ccxt.bitget()
+bithumb = ccxt.bithumb()
+bitmart = ccxt.bitmart()
+bitmex = ccxt.bitmex()
+bitstamp = ccxt.bitstamp()
+kucoin = ccxt.kucoin()
+binance = ccxt.binance()
