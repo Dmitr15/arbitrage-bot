1424ae620b355f27993af55bede489f9b8137c0b
DM1 <dmitroy1234@gmail.com>
2025-01-30 23:00:02 +0300

    Deleting extra imports

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import pprint
-
-import ccxt
-import pandas as pd
-import pybit
-from pybit.unified_trading import HTTP
 from arbitrage import arb_oppotunities
 
+#Arbitrage opportunities detection app
 
 if __name__ == '__main__':
     arb_oppotunities()
