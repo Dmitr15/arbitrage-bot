dd9e144dfb6f36706236fc7d9117d83833eac73d
DM1 <dmitroy1234@gmail.com>
2025-01-30 22:33:23 +0300

    Addition exchanges

---
+++ exchanges.py
@@ -0,0 +1,15 @@
+import ccxt
+
+#Necessary exchanges
+bingx = ccxt.bingx()
+bitbank = ccxt.bitbank()
+bitfinex = ccxt.bitfinex()
+bitget = ccxt.bitget()
+bithumb = ccxt.bithumb()
+bitmart = ccxt.bitmart()
+bitmex = ccxt.bitmex()
+bitstamp = ccxt.bitstamp()
+kucoin = ccxt.kucoin()
+binance = ccxt.binance()
+
+needed_exchanges = [bingx, bitbank, bitfinex, bitget, bithumb, bitmart, bitmex, bitstamp, kucoin, binance]
